94945c8919abe6200e7a045ca6ff717cb0ec5804
Nicholas <njc3006@rit.edu>
2015-04-21 15:07:55 -0400

    Added the correct class to extend, and added some super constructor calls, but then hit a road block

--- src/main/java/betteranvils/BetterAnvils.java
+++ src/main/java/betteranvils/BetterAnvils.java
@@ -1,18 +1,35 @@
 package betteranvils;
-
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.ContainerRepair;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry; 
 
 @Mod(modid = BetterAnvils.MODID, version = BetterAnvils.VERSION, name = "Better Anvils")
-public class BetterAnvils {
+public class BetterAnvils extends ContainerRepair{
 
 	public static final String MODID = "betteranvils";
     public static final String VERSION = "1.0";
+    
+    public BetterAnvils(InventoryPlayer playerInventory, World worldIn,
+			EntityPlayer player) {
+		super(playerInventory, worldIn, player);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BetterAnvils(InventoryPlayer playerInventory, World worldIn,
+			BlockPos blockPosIn, EntityPlayer player) {
+		super(playerInventory, worldIn, blockPosIn, player);
+		// TODO Auto-generated constructor stub
+        
+	}
     
     public void init(FMLInitializationEvent event){
         //System.out.println("Starting Better Anvils Mod");
@@ -34,6 +51,8 @@ public class BetterAnvils {
     public void removeXPfromRepair(){
   	
     }
+    
+    
     
 }//end better anvils
 
