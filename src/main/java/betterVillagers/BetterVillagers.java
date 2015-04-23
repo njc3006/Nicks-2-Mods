2a52d245f2597b6e8f442f37076c492213c64489
Nick boa <nsb8362@g.rit.edu>
2015-04-23 14:37:45 -0400

    added new mod stubs
    added new mod stubs for More Ores and better Villagers
    fixed an miss naming in better Villages

--- src/main/java/betterVillagers/BetterVillagers.java
+++ src/main/java/betterVillagers/BetterVillagers.java
@@ -1,17 +1,18 @@
-package betterVillages;
+package betterVillagers;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import betteranvils.BetterAnvils;
 
-@Mod(modid = BetterVillages.MODID, version = BetterVillages.VERSION, name = "Better Villages")
-public class BetterVillages {
+@Mod(modid = BetterVillagers.MODID, version = BetterVillagers.VERSION, name = "Better Villagers")
+public class BetterVillagers {
 
-	public static final String MODID = "bettervillages";
+	public static final String MODID = "bettervillagers";
     public static final String VERSION = "1.0";
     @EventHandler
     public void init(FMLInitializationEvent event){
-        System.out.println("Starting Better Villages Mod");
+        System.out.println("Starting Better Villagers Mod");
         makeChanges();
     }
 	private void makeChanges() {
--- src/main/java/moreOres/MoreOres.java
+++ src/main/java/moreOres/MoreOres.java
@@ -1,17 +1,16 @@
-package betterVillages;
+package moreOres;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = BetterVillages.MODID, version = BetterVillages.VERSION, name = "Better Villages")
-public class BetterVillages {
-
-	public static final String MODID = "bettervillages";
+@Mod(modid = MoreOres.MODID, version = MoreOres.VERSION, name = "More Ores")
+public class MoreOres {
+	public static final String MODID = "moreores";
     public static final String VERSION = "1.0";
     @EventHandler
     public void init(FMLInitializationEvent event){
-        System.out.println("Starting Better Villages Mod");
+        System.out.println("Starting More Ores Mod");
         makeChanges();
     }
 	private void makeChanges() {
